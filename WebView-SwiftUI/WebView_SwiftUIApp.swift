6c582ac339236eed6e8513a3cdda4d56f69ec4b2
Fatih <fatihk.96brs@gmail.com>
2025-02-12 01:53:11 +0300

    Initial Commit

---
+++ WebView-SwiftUI/WebView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_SwiftUIApp.swift
+//  WebView-SwiftUI
+//
+//  Created by Fatih Kenarda on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebView-SwiftUITests/WebView_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_SwiftUITests.swift
+//  WebView-SwiftUITests
+//
+//  Created by Fatih Kenarda on 12.02.2025.
+//
+
+import Testing
+@testable import WebView_SwiftUI
+
+struct WebView_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
